95e5c1236bb5ec8fea7d11853c08f0b7bec88a07
fucheng830 <fucheng830@sina.com>
2015-05-22 17:01:50 +0800

    加入股票画图模块

---
+++ src/fucheng/url.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+'''
+Created on 2015年5月22日
+
+@author: FUCHENG  fucheng830@sina.com
+'''
+
+class GetPage(object):
+    '''
+          获取网页类
+    
+    '''
+
+
+    def __init__(self, params):
+        '''
+                    构造函数
+        '''
+    
+    def open_url(self,url):
+            
--- src/test.py
+++ src/test.py
@@ -15,6 +15,8 @@ pl.plot(data)
 pl.show()
 '''
 
+
+'''
 def c(p,last=5,g=1):
     last=g*5
     if p>last:
@@ -26,3 +28,36 @@ def c(p,last=5,g=1):
         return p,last,g
 
 print c(18) 
+'''
+
+import urllib   
+import urllib2   
+     
+request_url = 'http://www.sina.com.cn' 
+
+user_headers = {
+    'x-requestted-with': 'XMLHttpRequest',
+    'Cache-Control': 'no-cache',
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1',  
+    'Connection' : 'Keep-Alive',
+}
+   
+
+
+req = urllib2.Request(url=request_url,headers=user_headers)   
+response = urllib2.urlopen(req)   
+the_page = response.read()
+
+#the_page.decode('gbk')  
+'''
+def unzip(data):
+        import gzip
+        import StringIO
+        data = StringIO.StringIO(data)
+        gz = gzip.GzipFile(fileobj=data)
+        data = gz.read()
+        gz.close()
+        return data
+'''
+print the_page
+
